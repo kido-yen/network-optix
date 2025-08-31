03a96e23fb17df992db0e19eda91f1bf046938e0
root <root@Kido-P7510.localdomain>
2025-08-31 20:03:44 +0800

    Upgrade to ubuntu 24.04 download deb file from internet directly

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,5 +2,5 @@
 CUSTOMIZATION=$1
 CUSTOMIZATION=${CUSTOMIZATION:-networkoptix}
 BASEDIR=/opt/$CUSTOMIZATION/mediaserver
-sudo ${BASEDIR}/bin/root-tool-bin&
-sudo ${BASEDIR}/bin/mediaserver-bin  -e
+sudo ${BASEDIR}/bin/root-tool &
+sudo ${BASEDIR}/bin/mediaserver  -e
